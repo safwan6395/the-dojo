7b83011247d7b26de9d1ea81c2566474ac40dbfd
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-08-10 15:57:32 +0500

    Setup pages and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// Styles
+import "./App.css";
+
+// Pages and components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+// styles
+import './Dashboard.css'
+
+function Dashboard() {
+  return (
+    <div>Dashboard</div>
+  )
+}
+export default Dashboard
