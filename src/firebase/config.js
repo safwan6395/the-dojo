24f073001496f751b74e9bbd0bbddc0db774b6b6
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-08-10 15:08:40 +0500

    Setup firebase on the frontend

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDe9G9W5jBHxesENFLmxs5LlOoBQsKlpOk",
+  authDomain: "thedojosite-v1.firebaseapp.com",
+  projectId: "thedojosite-v1",
+  storageBucket: "thedojosite-v1.appspot.com",
+  messagingSenderId: "98641213501",
+  appId: "1:98641213501:web:6e24352ca31da754ced254",
+};
+
+// Init firebase
+firebase.initializeApp(firebaseConfig);
+
+// Init individual services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// Timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
