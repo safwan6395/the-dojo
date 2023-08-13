3fd9d12b6909e23d08a6fc61b58ce0223edd7404
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-08-13 23:20:57 +0500

    Create the Signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,49 @@
 // styles
-import './Signup.css'
+import { useState } from "react";
+import "./Signup.css";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
   return (
-    <div>Signup</div>
-  )
+    <form className='auth-form'>
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          required
+          type='text'
+          onChange={(e) => setEmail(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile Thumbnail:</span>
+        <input required type='file' />
+      </label>
+      <button className='btn'>Sign up</button>
+    </form>
+  );
 }
-export default Signup
+export default Signup;
